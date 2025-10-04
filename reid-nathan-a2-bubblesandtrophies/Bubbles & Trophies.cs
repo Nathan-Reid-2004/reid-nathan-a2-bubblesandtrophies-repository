97b8ae8b90b69ae363ae572de5543333b49b3589
nreid <nreid@BOBICKLE>
2025-10-04 00:19:19 -0400

    Add Array & Loop for Bubble Position/Radius, Add Function for Drawing Wall, Add Function for Drawing Bomb, Add Function and Mouse Input tracking for Pointer, Add Custom Colours, Recorganized Project.

--- reid-nathan-a2-bubblesandtrophies/Bubbles & Trophies.cs
+++ reid-nathan-a2-bubblesandtrophies/Bubbles & Trophies.cs
@@ -1,6 +1,7 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
 using System.Numerics;
+using System.Reflection.Metadata;
 
 // The namespace your code is in.
 namespace MohawkGame2D
@@ -20,7 +21,24 @@ namespace MohawkGame2D
         {
             Window.SetTitle ("Bubbles & Trophies");
             Window.SetSize(400, 400);
-            Window.TargetFPS = 1;
+            Window.TargetFPS = 60;
+
+            //bubbles radius and position picker
+            int maxBubbles = 25;
+            Vector2[] randomBubbles = new Vector2[maxBubbles];
+
+            for (int i = 0; i < maxBubbles; i++)
+            {
+                randomBubbles[i] = Random.Vector2(380, 380);
+                float bubblesRadius = Random.Float(25);
+
+
+                //Draw.FillColor = bubbleBlue;
+                //Draw.LineSize = 1;
+                //Draw.LineColor = Color.White;
+                //Draw.Circle(randomBubbles[i], bubblesRadius);
+
+            }
         }
 
         /// <summary>
@@ -28,24 +46,83 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            //clear screen every frame
             Window.ClearBackground(Color.Gray);
 
+            //making the pointer click colour
+            Color pointerOrange = new Color("#FFAD00");
 
-            Draw.Line(new Vector2(20, 20), new Vector2(380, 20));
-            Draw.LineSize = 3;
-            Draw.LineColor = Color.Red;
+            //making the bubble colour
+            Color bubbleBlue = new Color(203, 239, 255);
 
-            Draw.Line(new Vector2(20, 20), new Vector2(20, 380));
-            Draw.LineSize = 3;
-            Draw.LineColor = Color.Red;
+            //drawing various functions (functions listed below)
+            drawWall();
+            drawBomb();
 
-            Draw.Line(new Vector2(20, 380), new Vector2(380, 380));
-            Draw.LineSize = 3;
-            Draw.LineColor = Color.Red;
 
-            Draw.Line(new Vector2(380, 380), new Vector2(380, 20));
+            //bomb main rectangle & small decor rectanges within the bomb
+            void drawBomb()
+            {
+                Draw.FillColor = Color.Red;
+                Draw.Rectangle(new Vector2(160, 160), new Vector2(80, 80));
+
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(new Vector2(160, 220), new Vector2(20, 20));
+
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(new Vector2(180, 200), new Vector2(20, 20));
+
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(new Vector2(200, 180), new Vector2(20, 20));
+
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(new Vector2(220, 160), new Vector2(20, 20));
+
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(new Vector2(160, 160), new Vector2(20, 20));
+
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(new Vector2(220, 220), new Vector2(20, 20));
+            }
+
+            // player pointer
+            if (Input.IsMouseButtonDown(MouseInput.Left))
+            {
+                Draw.FillColor = pointerOrange;
+            }
+            else
+            {
+                Draw.FillColor = Color.Blue;
+            }
+            Draw.LineColor = Color.Black;
             Draw.LineSize = 3;
-            Draw.LineColor = Color.Red;
+            Draw.Circle(Input.GetMouseX(), Input.GetMouseY(), 10);
+
+
+
+            // wall border lines
+            void drawWall()
+            {
+                Draw.LineSize = 3;
+                Draw.LineColor = Color.Red;
+                Draw.Line(new Vector2(20, 20), new Vector2(380, 20));
+
+                Draw.LineSize = 3;
+                Draw.LineColor = Color.Red;
+                Draw.Line(new Vector2(20, 20), new Vector2(20, 380));
+
+                Draw.LineSize = 3;
+                Draw.LineColor = Color.Red;
+                Draw.Line(new Vector2(20, 380), new Vector2(380, 380));
+
+                Draw.LineSize = 3;
+                Draw.LineColor = Color.Red;
+                Draw.Line(new Vector2(380, 380), new Vector2(380, 20));
+            }
+
+            
+
+
         }
     }
 
